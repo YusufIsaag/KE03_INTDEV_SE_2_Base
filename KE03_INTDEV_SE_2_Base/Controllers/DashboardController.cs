c5297e301ec50ff15cd32bc3118ace6653335c53
Abdulrahim Isaag <2303668isaag@zuyd.nl>
2025-06-16 15:54:11 +0200

    1

---
+++ KE03_INTDEV_SE_2_Base/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KE03_INTDEV_SE_2_Base.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
